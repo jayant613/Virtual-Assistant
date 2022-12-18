ff5c1b9a317c22763df9af16f7fc8a8e27646758
Jayant Deol <jayantdeol5@gmail.com>
2022-12-18 07:53:39 +0530

    Virtual Assistant

--- virtual_assitant.cpp
+++ virtual_assitant.cpp
@@ -2,9 +2,31 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctime>
+#include <ctime> // must include this header file to use time function
 
 using namespace std;
+
+// function to wish user according to time
+
+void wishme()
+{
+    // current date and time based on your system timezone
+    time_t now = time(0);
+    tm *time = localtime(&now);
+
+    if (time->tm_hour < 12)
+    {
+        cout << "Good Morning Boss" << endl;
+    }
+    else if (time->tm_hour < 12 && time->tm_hour <= 12)
+    {
+        cout << "Good Afternoon Boss" << endl;
+    }
+    else if (time->tm_hour > 16 && time->tm_hour <= 24)
+    {
+        cout << "Good Evening Boss" << endl;
+    }
+}
 
 int main()
 {
@@ -29,7 +51,28 @@ int main()
 
         if (strcmp(password, "inertia") == 0)
         {
-            cout << "Correct Password" << endl;
+            cout << "\n<*************************************************************************************>/n" << endl;
+            wishme();
+            do
+            {
+                cout << "\n<*************************************************************************************>/n" << endl;
+                cout << endl
+                     << "How can i help you....." << endl
+                     << endl;
+                cout << "Your Query --->";
+                gets(ch);
+                cout << endl;
+                cout << "Here is the result for your query --->";
+
+                if (strcmp(ch, "hi") == 0 || strcmp(ch, "hey") == 0 || strcmp(ch, "hello") == 0)
+                {
+                    cout << "Hello Boss........" << endl;
+                }
+                if (strcmp(ch, "hi") == 0 || strcmp(ch, "hey") == 0 || strcmp(ch, "hello") == 0)
+                {
+                    cout << "Hello Boss........" << endl;
+                }
+            } while (1);
         }
         else
         {
